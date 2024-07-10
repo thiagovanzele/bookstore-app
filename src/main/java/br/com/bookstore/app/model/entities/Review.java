dcf0d451415ec4d2797b6c2548b2be26e4bb1a6c
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-10 15:36:13 -0300

    Added Hashcode and Equals and Serializable in Review entity

--- src/main/java/br/com/bookstore/app/model/entities/Review.java
+++ src/main/java/br/com/bookstore/app/model/entities/Review.java
@@ -1,5 +1,8 @@
 package br.com.bookstore.app.model.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +11,8 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_review")
-public class Review {
+public class Review implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,6 +44,25 @@ public class Review {
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(comment, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Review other = (Review) obj;
+		return Objects.equals(comment, other.comment) && Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 	
