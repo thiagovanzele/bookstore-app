b38eb052a14467b68d74160e5ed2e441699adc7a
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-10 15:32:05 -0300

    Review entity created

---
+++ src/main/java/br/com/bookstore/app/model/entities/Review.java
@@ -0,0 +1,11 @@
+package br.com.bookstore.app.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_review")
+public class Review {
+
+	
+}
