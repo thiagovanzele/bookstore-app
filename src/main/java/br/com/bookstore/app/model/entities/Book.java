3f170aeb8a1b61221f6f9736c70de242e5f80a43
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-10 15:39:57 -0300

    Book entity created

---
+++ src/main/java/br/com/bookstore/app/model/entities/Book.java
@@ -0,0 +1,67 @@
+package br.com.bookstore.app.model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_book")
+public class Book implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+
+	public Book() {
+		super();
+	}
+
+	public Book(String title) {
+		super();
+		this.title = title;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id) && Objects.equals(title, other.title);
+	}
+	
+	
+	
+}
