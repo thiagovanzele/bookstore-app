ad1e170efc7afc03451624e52b687fad6d0b7c8d
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-10 15:35:01 -0300

    Publisher entity created

--- src/main/java/br/com/bookstore/app/model/entities/Review.java
+++ src/main/java/br/com/bookstore/app/model/entities/Review.java
@@ -1,11 +1,47 @@
 package br.com.bookstore.app.model.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_review")
 public class Review {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String comment;
+
+	public Review() {
+		super();
+	}
+
+	public Review(String comment) {
+		super();
+		this.comment = comment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	
 	
 }
