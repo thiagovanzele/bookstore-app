167b05727075829c2da27be123dacdb58f6942d8
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-10 16:21:11 -0300

    Review service created

---
+++ src/main/java/br/com/bookstore/app/model/services/ReviewService.java
@@ -0,0 +1,42 @@
+package br.com.bookstore.app.model.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.bookstore.app.model.entities.Review;
+import br.com.bookstore.app.model.repositories.ReviewRepository;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	private ReviewRepository repository;
+	
+	public Optional<Review> findReviewById(Long id) {
+		return repository.findById(id);
+	}
+	
+	public List<Review> findAll() {
+		return repository.findAll();
+	}
+	
+	public Review insertReview(Review review) {
+		return repository.save(review);
+	}
+	
+	public Review updateReview(Long id, Review obj) {
+		Review review = repository.getReferenceById(id);
+		updateData(review, obj);
+		return repository.save(review);
+	}
+
+	private void updateData(Review review, Review obj) {
+		review.setComment(obj.getComment());
+		review.setBook(review.getBook());
+	}
+
+	
+}
