337af1587179731e6007f9b0c91ace10d63c5f23
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-10 17:00:08 -0300

    Author service and controller created

---
+++ src/main/java/br/com/bookstore/app/controllers/AuthorController.java
@@ -0,0 +1,47 @@
+package br.com.bookstore.app.controllers;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.bookstore.app.model.entities.Author;
+import br.com.bookstore.app.model.services.AuthorService;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+	@Autowired
+	private AuthorService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Author>> findAuthorById(@PathVariable Long id) {
+		Optional<Author> obj = service.findAuthorById(id);
+		return ResponseEntity.ok(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Author>> findAll() {
+		List<Author> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Author> insert(@RequestBody Author obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+}
---
+++ src/main/java/br/com/bookstore/app/model/services/AuthorService.java
@@ -0,0 +1,39 @@
+package br.com.bookstore.app.model.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.bookstore.app.model.entities.Author;
+import br.com.bookstore.app.model.repositories.AuthorRepository;
+
+@Service
+public class AuthorService {
+
+	@Autowired
+	private AuthorRepository repository;
+	
+	public Optional<Author> findAuthorById(Long id) {
+		return repository.findById(id);
+	}
+	
+	public List<Author> findAll() {
+		return repository.findAll();
+	}
+	
+	public Author insert(Author obj) {
+		return repository.save(obj);
+	}
+	
+	public Author update(Long id, Author obj) {
+		Author author = repository.getReferenceById(id);
+		updateData(author, obj);
+		return repository.save(author);
+	}
+
+	private void updateData(Author author, Author obj) {
+		author.setName(obj.getName());
+	}
+}
