e7609d35abd1d7c6ba4c8b99ca1ee8cad61f8205
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-10 16:04:54 -0300

    Book repository created

---
+++ src/main/java/br/com/bookstore/app/model/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package br.com.bookstore.app.model.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.bookstore.app.model.entities.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+	public Book findBookByTitleContainingIgnoreCase(String title);
+}
